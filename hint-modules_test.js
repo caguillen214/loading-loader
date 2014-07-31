aacf4774bfe44b224bff38191f3a67ceaafee99c
Carlo s A. Guillen <caguillen@google.com>
2014-07-31 10:58:22 -0700

    refactor(hint-modules): remove custom levenshtein and refactor to use suggest-it

--- hint-modules_test.js
+++ hint-modules_test.js
@@ -8,7 +8,8 @@ describe('hintModules', function() {
       'testModule': {name:'createdAndNotLoaded', requires: []}
     }
     modData.loadedModules = {
-      'doesntExist': 'doesntExist'
+      'doesntExist': 'doesntExist',
+      'testModule': 'testModule'
     }
     modData.createdMulti = {
       'testModule': 'testModule'
@@ -22,11 +23,21 @@ describe('hintModules', function() {
   it('should identify modules loaded that do not exsist', function() {
     angular.module('testModule', ['doesntExist']);
     start();
-    expect(Object.keys(hintLog.flush()['Modules'])[2]).toBe(' Module "doesntExist" was loaded but does not exist.');
+    expect(Object.keys(hintLog.flush()['Modules'])[1]).toBe(' Module "doesntExist" was loaded but does not exist.');
   });
   it('should identify modules that have been loaded multiple times', function() {
     angular.module('testModule', []);
     start();
-    expect(Object.keys(hintLog.flush()['Modules'])[3]).toBe(' Multiple modules with name "testModule" are being created and they will overwrite each other.');
+    expect(Object.keys(hintLog.flush()['Modules'])[2]).toBe(' Multiple modules with name "testModule" are being created and they will overwrite each other.');
+  });
+  it('should ignore modules loaded twice if one is just being called', function() {
+    angular.module('testModule2', []);
+    angular.module('testModule2').controller('controller', [function(){}]);
+    start();
+    var results = Object.keys(hintLog.flush()['Modules']);
+    var finResult = results.some(function(res){
+      return res === " Multiple modules with name 'testModule' are being created and they will overwrite each other.";
+    });
+    expect(finResult).toBe(false);
   });
 });
